54c70ddd63f271dd601f9b123a3beeb4a3fd5b4f
boybenson <ybenson96@gmail.com>
2022-01-23 17:47:36 +0000

    feat: introduced the suspense method

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { unProtectedRoutes } from "./routes";
@@ -6,11 +6,13 @@ import { unProtectedRoutes } from "./routes";
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        {unProtectedRoutes.map(({ path, element }, index) => {
-          return <Route path={path} element={element} />;
-        })}
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {unProtectedRoutes.map(({ path, element }, index) => {
+            return <Route path={path} element={element} key={index} />;
+          })}
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
