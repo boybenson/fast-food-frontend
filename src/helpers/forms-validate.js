5a3ea52f866244e09aea458f56645299fe7f3ed6
boybenson <ybenson96@gmail.com>
2022-02-09 15:51:29 +0000

    feat: changes

--- src/helpers/forms-validate.js
+++ src/helpers/forms-validate.js
@@ -23,7 +23,7 @@ export const signUpFormValidate = (values) => {
   if (!values.phone) {
     errors.phone = "Phone Number Is Required";
   } else if (values.phone.length !== 10) {
-    errors.phone = "Phone Number Number should be 10";
+    errors.phone = "Phone number must be 10 digits";
   }
 
   if (!values.password) {
